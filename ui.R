ee1e8b0c2b344277ccd263e7a57b58a7143732a5
Raunak Shakya <rkshakya99@gmail.com>
2018-11-16 08:41:49 +0545

    initial commit

---
+++ ui.R
@@ -0,0 +1,9 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Developing Data Products Week 4 Assignment"),
+  
+  sidebarLayout(sidebarPanel(),
+                
+                mainPanel())
+))
