898ce8592a9c03be45044eb372de080402f973c6
Raunak Shakya <rkshakya99@gmail.com>
2018-11-17 16:52:54 +0545

    complete assignment

--- server.R
+++ server.R
@@ -1,5 +1,74 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
 
-shinyServer(function(input, output) {
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+  datasetInput <- reactive({
+    switch(
+      input$dataset,
+      "mtcars" = mtcars,
+      "iris" = iris,
+      "quakes" = quakes
+    )
+  })
+  
+  observe({
+    selectedDataset <- datasetInput()
+    
+    updateSelectInput(session, "xvar", choices = names(selectedDataset))
+    updateSelectInput(
+      session,
+      "yvar",
+      choices = names(selectedDataset),
+      selected = names(selectedDataset)[[2]]
+    )
+  })
+  
+  plotData <- reactive({
+    d <- datasetInput()
+    if (input$xvar %in% names(d) & input$yvar %in% names(d)) {
+      return(d[, c(input$xvar, input$yvar)])
+    }
+  })
+  
+  clusters <- reactive({
+    kmeans(plotData(), input$clusters, input$maxiter)
+  })
+  
+  output$plot1 <- renderPlot({
+    par(mar = c(5, 4, 0, 1))
+    p <- plotData()
+    req(p)
+    plot(p,
+         col = clusters()$cluster,
+         pch = 20,
+         cex = 2)
+    points(clusters()$centers,
+           pch = 8,
+           cex = 2,
+           lwd = 2)
+  })
+  
+  output$str_paragraph_title <- renderText({
+    HTML(paste(
+      "<h4>Structure of ",
+      "<b><i>",
+      input$dataset,
+      "</b></i>",
+      " dataset</h4>"
+    ))
+  })
+  
+  output$str_dataset <- renderPrint({
+    str(datasetInput())
+  })
   
 })
--- ui.R
+++ ui.R
@@ -1,9 +1,49 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Developing Data Products Week 4 Assignment"),
+  titlePanel("Shiny Application: K-means clustering project"),
   
-  sidebarLayout(sidebarPanel(),
-                
-                mainPanel())
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "dataset",
+        label = "Choose dataset",
+        choices = c("iris", "mtcars", "quakes")
+      ),
+      
+      selectInput('xvar', 'X variable', names(iris)),
+      
+      selectInput('yvar',
+                  'Y variable',
+                  names(iris),
+                  selected = names(iris)[[2]]),
+      
+      numericInput('clusters', 'Cluster count', 3, min = 1, max = 9),
+      
+      numericInput(
+        'maxiter',
+        'Maximum iterations',
+        10,
+        min = 1,
+        max = 100
+      ),
+      
+      uiOutput("ui")
+    ),
+    
+    mainPanel(
+      plotOutput('plot1'),
+      htmlOutput("str_paragraph_title"),
+      verbatimTextOutput("str_dataset")
+    )
+  )
 ))
